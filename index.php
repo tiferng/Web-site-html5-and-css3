e5ce7fbe0acba68e85e152377dc6e7237fd10187
Erraissi <37183209+tiferng@users.noreply.github.com>
2018-07-18 14:32:13 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="Scripts/style.css">
+	<style type="text/css">
+		body{
+			background-color: rgb(40,53,80);
+			background-position: top;
+		}
+	</style>
+</head>
+<body>
+
+<div class="main">
+	<div id="header">
+		<img src="image/images123.jpg" width="200" height="150">
+		<h2>Y.ERRAISI</h2>
+	</div>
+	<div class="menu" id="menu">
+		<ul>
+			<li><a href="">الرئيسية </a></li>
+			<li><a href="">مشاريع </a></li>
+			<li><a href="">اخبار </a></li>
+			<li><a href="">حول </a></li>
+			<li><a href="">اتصل بنا </a></li>
+		</ul>
+	</div>
+	<div>
+		<MARQUEE class="marq" widht=100% behavior=scroll direction=right> الموقع الرسمي لمطور ومصمم مواقع الويب الرايسي يوسف
+		</MARQUEE>
+	</div>
+	<div class="content">
+		<p> يوسف الرايسي الادريسي</p>
+		
+	</div>
+
+	<div class="footer">
+		<a href="">اتصل بنا</a>|
+		<a href="">من نحن</a>
+		<br/>
+		<a href="https://www.facebook.com/youssefi.erraici"><img class="svg" src="Scripts/facebook.svg"></a>
+		<a href="https://www.youtube.com/channel/UCZw6RJX5loyH1p6HcmF6PBg?view_as=subscriber"><img class="svg" src="Scripts/youtube.svg"></a>
+		<a href="https://www.linkedin.com/in/yusef-eraissi-778556153/"><img class="svg" src="Scripts/linkedin.svg"></a>
+		<br/>
+		<span>جميع الحقوق محفوضة لموقع الرايسي</span>
+	</div>
+</div>
+
+</body>
+</html>
